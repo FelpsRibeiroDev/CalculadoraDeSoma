9437ecbbdbe16f6ded8eeccc2377a3e031e8ee62
Felipe Ribeiro <felipe.dev2004@gmail.com>
2024-12-14 00:26:55 -0300

    Feature: Calculadora apenas de soma pronta.

---
+++ Somando.py
@@ -0,0 +1,8 @@
+print("    Somando 2 números    ")
+
+nm1 = input("Digite o primeiro número: ")
+nm2 = input("Digite o segundo número: ")
+
+soma = int(nm1) + int(nm2)
+
+print("O resultado da somatória é igual a", soma)
